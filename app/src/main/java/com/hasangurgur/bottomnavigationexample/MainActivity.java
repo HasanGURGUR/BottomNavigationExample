99c6c61ff480cf0544f52e6e4a2cfef10f1f6e11
Hasan GURGUR <hasangurgur95@gmail.com>
2021-07-25 16:40:00 +0300

    Initial commit

---
+++ app/src/main/java/com/hasangurgur/bottomnavigationexample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.hasangurgur.bottomnavigationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView1  =findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,R.id.fragmentContainerView);
+
+        NavigationUI.setupWithNavController(bottomNavigationView1, navController);
+
+
+
+
+
+    }
+}
